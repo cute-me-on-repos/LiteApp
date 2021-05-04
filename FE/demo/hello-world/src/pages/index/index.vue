1495c9e3ac26b03d3e40274546abd9406e72368d
Lý Lưỡng Thiên <thien.ly@tiki.vn>
2021-05-04 09:31:50 +0700

    fix: be able to run on new version of android

--- FE/demo/hello-world/src/pages/index/index.vue
+++ FE/demo/hello-world/src/pages/index/index.vue
@@ -1,22 +1,33 @@
 <template>
-    <div class="container">
-        <h1>
-            hello world~
-        </h1>
-    </div>
+  <div class="container">
+    <h1>
+      hello world~
+    </h1>
+    <video width="400" controls>
+      <source
+        src="https://www.w3schools.com/html/mov_bbb.mp4"
+        type="video/mp4"
+      />
+      <source
+        src="https://www.w3schools.com/html/mov_bbb.ogg"
+        type="video/ogg"
+      />
+      Your browser does not support HTML video.
+    </video>
+  </div>
 </template>
 <script>
-export default {}
+export default {};
 </script>
 <style lang="scss">
-.container{
-    width:100%;
-    padding:0 50px;
-    background:green;
-    h1{
-        font-size:20px;
-        font-weight:600;
-        color:orange;
-    }
+.container {
+  width: 100%;
+  padding: 0 50px;
+  background: green;
+  h1 {
+    font-size: 20px;
+    font-weight: 600;
+    color: orange;
+  }
 }
 </style>
